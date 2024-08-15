b751bf12881ca5f37e1e8f354ce984f7fc077114
jfcode101 <125421939+jfcode101@users.noreply.github.com>
2024-08-15 09:14:41 -0400

    updating the files to meet the requirements

--- r-alab-316-3-1/index.js
+++ r-alab-316-3-1/index.js
@@ -1,4 +1,4 @@
-// #4. adding menu interaction
+// #316.3.4 adding menu interaction
 var menuLinks = [
   { text: "about", href: "/about" },
   {
@@ -29,19 +29,19 @@ var menuLinks = [
   },
 ];
 
+// #3.16.1.1 Getting started
 const mainEl = document.querySelector("main");
 mainEl.style.backgroundColor = "var(--main-bg)";
 mainEl.innerHTML = `<h1>DOM Manipulation</h1>.`;
 mainEl.classList.add("flex-ctr");
 
-// create menu bar
+// #316.1.2 create menu bar
 const topMenuEl = document.querySelector("#top-menu");
 topMenuEl.style.height = "100%";
 topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
-
 topMenuEl.classList.add("flex-around");
 
-// iterate through the menu
+// #316.1.2 adding menu buttons — iterate through the menu
 menuLinks.forEach((link) => {
   const anchorEl = document.createElement("a");
   anchorEl.setAttribute("href", link.href);
@@ -49,9 +49,9 @@ menuLinks.forEach((link) => {
   topMenuEl.appendChild(anchorEl);
 });
 
-/*...... Second Part .....*/
+/* ************* #316.3 — second part ************* */
 
-// #3. creating the submenu
+// #316.3.3 creating the submenu
 const subMenuEl = document.querySelector("#sub-menu");
 subMenuEl.style.height = "100%";
 subMenuEl.style.backgroundColor = "var(--sub-menu-bg)";
@@ -59,35 +59,72 @@ subMenuEl.classList.add("flex-around");
 subMenuEl.style.position = "absolute";
 subMenuEl.style.top = "0";
 
-// #4. adding menu interaction
+// #313.3.5 attaching a delegate {click} event to subMenuEl
+subMenuEl.addEventListener("click", (e) => {
+  // prevent default
+  e.preventDefault();
+
+  // return if clicked element is not an anchor
+  if (e.target.tagName !== "A") return;
+  //   print it out to check if it's working
+  console.log(e.target.textContent);
+  // set the {topMenuEl} top property to
+});
+
+// #316.3.4 adding menu interaction
 // Select and cache all <a> elements inside of topMenuEl in variable topMenuLinks
 const topMenuLinks = topMenuEl.querySelectorAll("a");
 topMenuEl.addEventListener("click", (e) => {
   e.preventDefault();
+  // return if clicked element is not an anchor
   if (e.target.tagName !== "A") return;
   console.log(e.target.textContent);
 
-  //  #4.1  add event listener to add "active" class to <a></a>
-  // removing "active" class
+  // adding an event listener to either {add} or {remove} "active" class to the link <a>
   topMenuLinks.forEach((link) => {
-    link.classList.remove("active");
-    // console.log(link);
-  });
-  //  adding active class
-  e.target.classList.add("active");
-
-  // #5. add menu interaction ---- LAST CODE
-  if (topMenuLinks) {
-    // check if the clicked item is already active
-    if (!e.target.classList.contains("active")) {
-      // test console
-      console.log(
-        `${e.target.textContent.toUpperCase()} This anchor contains active`
-      );
-      // check if clicked <a> has a sublink
+    if (link === e.target) {
+      link.classList.toggle("active");
+    } else {
+      link.classList.remove("active");
     }
+  });
+
+  // #316.5. adding submenu interaction
+  /* #316.5.1 
+    we need a variable to store which anchor is clicked, to achieve this we will use find() method 
+     
+    find() — this method of Array instances returns the first 
+    element in the provided array that satisfies the provided testing function
+   */
+  const clickedAnchor = menuLinks.find(
+    (link) => link.text === e.target.textContent
+  );
+
+  // 316.5.1 check if the clicked anchor within the {menuLinks} has a {subLinks}
+  if (clickedAnchor.subLinks) {
+    // set the {subMenueEl} top property to 100%
+    subMenuEl.style.top = "100%";
+    // call the {buildSubmenu()} function
+    buildSubmenu(clickedAnchor.subLinks);
+  } else {
+    // set the {subMenueEl} top property to 0%
+    subMenuEl.style.top = "0";
   }
 });
 
-// test printing
-// console.log(subMenuEl);
+// function buildSubmenu
+function buildSubmenu(subLinks) {
+  // 1. clear the subMenuEL
+  subMenuEl.innerHTML = "";
+  // 2. forEach() loop to iterate over the {subLinks} array
+  subLinks.forEach((link) => {
+    // 2.a. create an <a> element
+    const subLinkAnchor = document.createElement("a");
+    // 2.b. set <href> attribute with value {link.href}
+    subLinkAnchor.setAttribute("href", link.href);
+    // 2.c. add text to {subLinkAnchor}
+    subLinkAnchor.textContent = link.text;
+    // 2.d. append the new element
+    subMenuEl.appendChild(subLinkAnchor);
+  });
+}
